5caaae20c6954fdedf80facfcc0e953af45c22ad
xsxinyi <xsxinyi@outlook.com>
2024-03-19 23:12:28 +0800

    完成 QPSK 在无噪声情况下的仿真例子

--- examples/QPSK_detail.m
+++ examples/QPSK_detail.m
@@ -13,14 +13,16 @@ sps = 16;%每个符号的采样点数, sample per symbol
 fc = 2000; %载波频率
 fs = symbol_rate * sps; %采样频率
 rollof_factor = 0.8;
-rcos_fir = rcosdesign(rollof_factor, 6, sps);
+span = 6;
+rcos_fir = rcosdesign(rollof_factor, span, sps);
+fir_M = 128;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%   信源    %%%%%%%%%%%%%%%%%%%
 %%%%% 随机信号 
 % msg_source = [ones(1,20) zeros(1,20) randi([0,1], 1, 99960)];
 % msg_source = [ones(1,10) zeros(1,10) randi([0,1], 1, 10)];
 
-msg_source = [1 0 0 1 1 0 1 1 1 0 1 1 1 0 1 0 1 1 0 0];
+msg_source = [1 0 0 1 1 0 1 1 1 0 1 1 1 0 1 0 1 1 0 0, randi([0,1], 1, 20)];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%   发射机  %%%%%%%%%%%%%%%%%%%%
 bipolar_msg_source = 2*msg_source-1;
@@ -97,15 +99,15 @@ title('Q路通过平方根升余弦成型滤波器后的时域波形');
 
 %%%%%载波发送
 time = 1:length(rcos_msg_source_I);
-tra_IFsignal = rcos_msg_source_I.*cos(2*pi*fc.*time/fs) - rcos_msg_source_I.*sin(2*pi*fc.*time/fs);
+tra_IFsignal = rcos_msg_source_I.*cos(2*pi*fc.*time/fs) - rcos_msg_source_Q.*sin(2*pi*fc.*time/fs);
 
 figure(15);
 plot(tra_IFsignal);
 title('上载波后的时域波形');
-figure(16);
-[H6,w6] = freqz(tra_IFsignal, 1, 'whole');
-plot(w6/pi,abs(H6));
-title('上载波后的频域波形');
+% figure(16);
+% [H6,w6] = freqz(tra_IFsignal, 1, 'whole');
+% plot(w6/pi,abs(H6));
+% title('上载波后的频域波形');
 
 
 %%%%%%相干解调
@@ -116,41 +118,41 @@ figure(17)
 plot(rcsv_msg_source_I);
 % stem(rcos_msg_source_addnoise,'.');
 title('相干解调后的I路时域波形');
-figure(18);
-[H7_I,w7_I] = freqz(rcsv_msg_source_I, 1, 'whole');
-plot(w7_I/pi,abs(H7_I));
-title('相干解调后的I路频域波形');
+% figure(18);
+% [H7_I,w7_I] = freqz(rcsv_msg_source_I, 1, 'whole');
+% plot(w7_I/pi,abs(H7_I));
+% title('相干解调后的I路频域波形');
 
 figure(19)
 plot(rcsv_msg_source_Q);
 % stem(rcos_msg_source_addnoise,'.');
 title('相干解调后的Q路时域波形');
-figure(20);
-[H7_Q,w7_Q] = freqz(rcsv_msg_source_Q, 1, 'whole');
-plot(w7_Q/pi,abs(H7_Q));
-title('相干解调后的I路频域波形');
+% figure(20);
+% [H7_Q,w7_Q] = freqz(rcsv_msg_source_Q, 1, 'whole');
+% plot(w7_Q/pi,abs(H7_Q));
+% title('相干解调后的I路频域波形');
 
-fir_lp = fir1(128, 0.2);
+fir_lp = fir1(fir_M, 0.2);
 rcsv_msg_source_lp_I = filter(fir_lp, 1, rcsv_msg_source_I);
 rcsv_msg_source_lp_Q = filter(fir_lp, 1, rcsv_msg_source_Q);
 
 figure(21)
-plot(rcsv_msg_source_lp_I);
-% stem(rcos_msg_source_addnoise,'.');
+% plot(rcsv_msg_source_lp_I);
+stem(rcsv_msg_source_lp_I,'.');
 title('FIR低通滤波后的I路时域波形');
-figure(22);
-[H8_I,w8_I] = freqz(rcsv_msg_source_lp_I, 1, 'whole');
-plot(w8_I/pi,abs(H8_I));
-title('FIR低通滤波后的I路频域波形');
+% figure(22);
+% [H8_I,w8_I] = freqz(rcsv_msg_source_lp_I, 1, 'whole');
+% plot(w8_I/pi,abs(H8_I));
+% title('FIR低通滤波后的I路频域波形');
 
 figure(23)
-plot(rcsv_msg_source_lp_Q);
-% stem(rcos_msg_source_addnoise,'.');
+% plot(rcsv_msg_source_lp_Q);
+stem(rcsv_msg_source_lp_Q,'.');
 title('FIR低通滤波后的Q路时域波形');
-figure(24);
-[H8_Q,w8_Q] = freqz(rcsv_msg_source_lp_Q, 1, 'whole');
-plot(w8_Q/pi,abs(H8_Q));
-title('FIR低通滤波后的I路频域波形');
+% figure(24);
+% [H8_Q,w8_Q] = freqz(rcsv_msg_source_lp_Q, 1, 'whole');
+% plot(w8_Q/pi,abs(H8_Q));
+% title('FIR低通滤波后的I路频域波形');
 
 rcsv_msg_source_MF_I = filter(rcos_fir, 1, rcsv_msg_source_lp_I);
 rcsv_msg_source_MF_Q = filter(rcos_fir, 1, rcsv_msg_source_lp_Q);
@@ -159,19 +161,66 @@ figure(25)
 % plot(rcsv_msg_source_MF_I);
 stem(rcsv_msg_source_MF_I,'.');
 title('均衡滤波后的I路时域波形');
-figure(26);
-[H9_I,w9_I] = freqz(rcsv_msg_source_MF_I, 1, 'whole');
-plot(w9_I/pi,abs(H9_I));
-title('均衡滤波后的I路频域波形');
+% figure(26);
+% [H9_I,w9_I] = freqz(rcsv_msg_source_MF_I, 1, 'whole');
+% plot(w9_I/pi,abs(H9_I));
+% title('均衡滤波后的I路频域波形');
 
 figure(27)
 % plot(rcsv_msg_source_MF_Q);
 stem(rcsv_msg_source_MF_Q,'.');
 title('均衡滤波后的Q路时域波形');
-figure(28);
-[H9_Q,w9_Q] = freqz(rcsv_msg_source_MF_Q, 1, 'whole');
-plot(w9_Q/pi,abs(H9_Q));
-title('均衡滤波后的I路频域波形');
+% figure(28);
+% [H9_Q,w9_Q] = freqz(rcsv_msg_source_MF_Q, 1, 'whole');
+% plot(w9_Q/pi,abs(H9_Q));
+% title('均衡滤波后的I路频域波形');
 
+decision_site = fir_M / 2 + span * sps;
+% display(decision_site);
+rcsv_msg_source_MF_option_I = rcsv_msg_source_MF_I(decision_site:sps:end);
+rcsv_msg_source_MF_option_Q = rcsv_msg_source_MF_Q(decision_site:sps:end);
+figure(29)
+% plot(rcos_msg_source_addnoise);
+stem(rcsv_msg_source_MF_option_I,'.');
+title('I路经过延时处理并抽取后的接收时域波形');
+% figure(30);
+% [H10_I,w10_I] = freqz(rcsv_msg_source_MF_option_I, 1, 'whole');
+% plot(w10_I/pi,abs(H10_I));
+% title('I路经过延时处理并抽取后的接收频域波形');
+figure(31)
+% plot(rcos_msg_source_addnoise);
+stem(rcsv_msg_source_MF_option_Q,'.');
+title('Q路经过延时处理并抽取后的接收时域波形');
+% figure(32);
+% [H10_Q,w10_Q] = freqz(rcsv_msg_source_MF_option_Q, 1, 'whole');
+% plot(w10_Q/pi,abs(H10_Q));
+% title('Q路经过延时处理并抽取后的接收频域波形');
+msg_source_MF_option_sign_I = sign(rcsv_msg_source_MF_option_I);
+msg_source_MF_option_sign_Q = sign(rcsv_msg_source_MF_option_Q);
+% figure(33);
+% stem(msg_source_MF_option_sign_I, '-*');
+% title('I路判决结果');
+% figure(34);
+% stem(msg_source_MF_option_sign_Q, '-*');
+% title('Q路判决结果');
+
+%%%%%%%%%%%%%%%%%%% 合并 I路 和 Q路
+length_I = length(msg_source_MF_option_sign_I);
+length_Q = length(msg_source_MF_option_sign_Q);
+% 初始化结果数组
+bipolar_combinedArray = zeros(1, length_I + length_Q);
+% 交替合并
+for i = 1:max(length_I, length_Q)
+if i <= length_I
+bipolar_combinedArray(2*i - 1) = msg_source_MF_option_sign_I(i);
+end
+if i <= length_Q
+bipolar_combinedArray(2*i) = msg_source_MF_option_sign_Q(i);
+end
+end
+combinedArray = (bipolar_combinedArray + 1) / 2;
+% 结果数组
+disp(combinedArray);
+stem(combinedArray, '-*');
 
 
