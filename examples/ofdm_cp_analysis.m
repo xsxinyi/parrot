9bb45a79f99c5ac9e3e1010366f830a5e39a79db
xsxinyi <xsxinyi@outlook.com>
2024-04-14 23:23:22 +0800

    修改 CP 相关仿真

---
+++ examples/ofdm_cp_analysis.m
@@ -0,0 +1,109 @@
+clc;
+clear;
+close all;
+
+% 定义时域样本点数量和采样间隔
+N = 1024; % 样本数量
+T = 1/1000; % 采样间隔，对应1000Hz的采样频率
+
+
+%% 创建时域矩形信号 (boxcar function)
+t = (-N/2:N/2-1)*T; % 时域坐标（考虑到对称性和0频分量位置）
+rect_width = 100*T; % 矩形信号的宽度，例如100个采样间隔
+rect_win = double(abs(t) < rect_width/2); % 生成矩形波形
+% use 20 dot for cp.
+rect_win = circshift(rect_win, 70);
+% plot(t, rect_win);
+rect_win2 = circshift(rect_win, 100);
+
+% 对 sin 函数加矩形窗
+f = 20;
+rect_sin_signal1 = sin((t-0.02)*f*2*pi).*rect_win;
+rect_sin_signal2 = sin((t-0.02)*1.5*f*2*pi).*rect_win;
+rect_sin_signal3 = sin((t-0.02)*2*f*2*pi).*rect_win;
+
+rect_sin_signa21 = -sin((t-0.02)*f*2*pi).*rect_win2;
+rect_sin_signa22 = sin((t-0.02)*1.5*f*2*pi).*rect_win2;
+rect_sin_signa23 = -sin((t-0.02)*2*f*2*pi).*rect_win2;
+
+%% 不加 CP 时
+figure(1);
+subplot(4,1,1);
+plot(t, rect_sin_signal1);
+hold on;
+plot(t, rect_sin_signal2);
+hold on;
+plot(t, rect_sin_signal3);
+hold on;
+plot(t, rect_sin_signa21);
+hold on;
+plot(t, rect_sin_signa22);
+hold on;
+plot(t, rect_sin_signa23);
+xlim([0, 0.3]);
+
+rect_sin_signal1 = rect_sin_signal1 + rect_sin_signal2 + rect_sin_signal3;
+rect_sin_signal2 = rect_sin_signa21 + rect_sin_signa22 + rect_sin_signa23;
+rect_sin_signal = rect_sin_signal1 + rect_sin_signal2;
+
+subplot(4,1,2);
+plot(t, rect_sin_signal);
+xlim([0, 0.3]);
+
+% 使用FFT将时域信号转换到频域
+X_rect_sin = fft(rect_sin_signal); % 进行FFT
+X_rect_sin_shifted = fftshift(X_rect_sin); % 频域信号以0Hz为中心（将直流分量移到中央）
+
+% 绘制频域信号
+subplot(4,1,3);
+Xf = (-N/2:N/2-1)*(1/(T*N)); % 频率坐标
+plot(Xf, abs(X_rect_sin_shifted) / max(abs(X_rect_sin_shifted))); % 取模并正规化至0至1
+
+title('Frequency - No CP');
+xlabel('Frequency (Hz)');
+ylabel('Normalized Magnitude');
+xlim([-100, 100]);
+
+% 绘制功率谱密度
+subplot(4,1,4);
+Xf = (-N/2:N/2-1)*(1/(T*N)); % 频率坐标
+plot(Xf, (abs(X_rect_sin_shifted).^2 / max(abs(X_rect_sin_shifted))^2)); % 取模并正规化至0至1
+xlim([-100, 100]);
+
+%% 加 CP 时
+add_cp_signal1 = rect_sin_signal1;
+add_cp_signal1(514:533) = rect_sin_signal1(613:632);
+% figure(11);
+% plot(t, add_cp_signal1);
+add_cp_signal2 = rect_sin_signal2;
+add_cp_signal2 = circshift(add_cp_signal2, 20);
+add_cp_signal2(633:652) = rect_sin_signal2(713:732);
+% figure(12);
+% plot(t, add_cp_signal2);
+
+add_cp_signal = add_cp_signal1 + add_cp_signal2;
+
+figure(2);
+subplot(3,1,1);
+plot(t, add_cp_signal);
+xlim([0, 0.3]);
+
+% 使用FFT将时域信号转换到频域
+X_cp = fft(add_cp_signal); % 进行FFT
+X_cp_shifted = fftshift(X_cp); % 频域信号以0Hz为中心（将直流分量移到中央）
+
+% 绘制频域信号
+subplot(3,1,2);
+Xf = (-N/2:N/2-1)*(1/(T*N)); % 频率坐标
+plot(Xf, abs(X_cp_shifted) / max(abs(X_cp_shifted))); % 取模并正规化至0至1
+title('Frequency - with CP');
+xlabel('Frequency (Hz)');
+ylabel('Normalized Magnitude');
+xlim([-100, 100]);
+
+subplot(3,1,3);
+Xf = (-N/2:N/2-1)*(1/(T*N)); % 频率坐标
+plot(Xf, abs(X_cp_shifted).^2 / max(abs(X_cp_shifted))^2); % 取模并正规化至0至1
+xlim([-100, 100]);
+
+
